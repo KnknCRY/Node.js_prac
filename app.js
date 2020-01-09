f9a14d36a2b17f7219006426c10ece111ad57bbe
KnknCRY <s35016080@gmail.com>
2020-01-09 09:46:37 +0800

    path,os,fs

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+
+/*path, os, fs
+const path = require('path');
+var pathObj = path.parse(__filename);
+console.log(pathObj);
+
+const os = require('os');
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+
+const fs = require('fs');
+var files = fs.readdirSync('./');
+console.log(files);
+fs.readdir('./123', function (err, files) {
+    if (err) console.log('Error', err);
+    else console.log('Result', files)
+});
+*/
+
+/*load module
 const logger = require("./logger.js");
 
-logger("hi");
+logger.log("hi");
+*/
--- logger.js
+++ logger.js
@@ -1,5 +1,9 @@
-function log(msg){
+console.log(__filename);
+console.log(__dirname);
+
+function log(msg) {
     console.log(msg);
 }
 
-module.exports = log;
+//module.exports = log;
+module.exports.log = log;
