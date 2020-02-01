45390714e150bd3a4a81d0ccf11120f30e9e6c8e
RueiYang <s35016080@gmail.com>
2020-02-01 21:46:45 +0800

    add jwt

--- vidly/models/user.js
+++ vidly/models/user.js
@@ -1,35 +1,48 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
-const User = mongoose.model('User', new mongoose.Schema({
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
     name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
     },
     email: {
-        type: String,
-        unique: true,
-        required: true,
-        minlength: 5,
-        maxlength: 255
+      type: String,
+      unique: true,
+      required: true,
+      minlength: 5,
+      maxlength: 255
     },
     password: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 1024
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024
     }
-}));
+  })
+);
 
 function validateUser(user) {
-    const schema = {
-        name: Joi.string().min(5).max(50).required(),
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
-    };
-    return Joi.validate(user, schema);
+  const schema = {
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+  };
+  return Joi.validate(user, schema);
 }
 
 module.exports.User = User;
--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,20 +1,29 @@
-const { User, validate } = require('./../models/user.js');
-const _ = require('lodash');
-const mongoose = require('mongoose');
-const express = require('express');
+const { User, validate } = require("./../models/user.js");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({ email: req.body.email });
-    if (user) return res.status(400).send('user already registered.');
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("user already registered.");
+  /*
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+*/
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
 
-    user = new User(_.pick(req.body, ['name','email','password']));
-    await user.save();
-
-    res.send(_.pick(user, ['_id', 'name', 'email']));
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
