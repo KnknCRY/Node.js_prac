67a719f95fb75a4ec3468f6cdff5b128ff6a99e1
RueiYang <s35016080@gmail.com>
2020-02-03 16:50:38 +0800

    add uncaughtException

--- vidly/index.js
+++ vidly/index.js
@@ -1,5 +1,6 @@
 require("express-async-errors"); //一定要在最上面require
 const winston = require("winston");
+require("winston-mongodb");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
@@ -14,7 +15,20 @@ const config = require("config");
 const error = require("./middleware/error");
 const app = express();
 
+process.on("uncaughtException", ex => {
+  console.log("We got uncaught exception");
+  winston.error(ex.message, ex);
+});
+
+//把錯誤訊息直接印出來到logfile.log，若檔案不存在會自動建立
 winston.add(winston.transports.File, { filename: "logfile.log" });
+//把錯誤訊息直接存在MongoDB，會自動建立一個log colection
+winston.add(winston.transports.MongoDB, {
+  db: "mongodb://localhost/vidly",
+  level: "info"
+});
+
+throw new Error("Something failed during startup.");
 
 //export vidly_jwtPrivateKey=mySecureKeyc
 if (!config.get("jwtPrivateKey")) {
